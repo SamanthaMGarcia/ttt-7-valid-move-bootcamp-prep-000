7f6ef1a3d4948a5575f3156f52b55f9f72b31b82
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-17 20:33:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 def valid_move?(board, index)
   if index.between?(0, 8) && !position_taken?(board, index)
     true
+  else
+    false
+  end
   #if board[index] <= 8
     #true
   #else position_taken
